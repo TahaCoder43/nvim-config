bffa4add36eea8926cdbb2785e2d0486526415fd
TahaCoder43 <TahaMunawar43@outlook.com>
2023-10-18 12:07:31 +0500

    space is represented by faded floating dot

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,5 +3,5 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
 vim.opt.list = true
-vim.opt.lcs = vim.opt.lcs + "space:."
+vim.opt.lcs = vim.opt.lcs + "space:꭪"
 vim.cmd("source ~/.config/nvim/lua/custom/macros.vim")
