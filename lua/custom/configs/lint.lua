db388fbc06803584cd7228a69f97d8082c226489
Taha <TahaMunawar43@outlook.com>
2024-06-20 22:15:48 +0500

    Added python linting with ruff and mypy

--- lua/custom/configs/lint.lua
+++ lua/custom/configs/lint.lua
@@ -1,10 +1,19 @@
+-- settings default linters
 require("lint").linters_by_ft = {
     javascript = {"eslint"},
     typescript = {"eslint"},
     javascriptreact = {"eslint"},
     typescriptreact = {"eslint"},
+    python = {"ruff", "mypy"},
 }
 
+-- adding command line arguments to be run with linters
+local mypy = require("lint").linters.mypy
+mypy.args = {
+    "--checked-untyped-defs"
+}
+
+-- setting the linter to be run, whenever we write a file
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
     callback = function()
         require("lint").try_lint()
