1f2b1c96c45be18f8fb466a8106072cfeaf805b6
Taha <TahaMunawar43@outlook.com>
2024-08-23 18:16:45 +0500

    Added markdown to pre_installed languages for tree sitter

--- lua/custom/configs/tree-sitter.lua
+++ lua/custom/configs/tree-sitter.lua
@@ -1,5 +1,5 @@
 local options = {
-  ensure_installed = { "lua", "rust", "python", "javascript", "typescript", "tsx", "rust", "go", "sql", "html", "bash", "csv", "yaml", "svelte" },
+  ensure_installed = { "lua", "rust", "python", "javascript", "typescript", "tsx", "rust", "go", "sql", "html", "bash", "csv", "yaml", "svelte", "markdown" },
 
   highlight = {
     enable = true,
